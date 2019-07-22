37de3453373a11b973821ba67a356dc91a406cf4
DavidStinghen <davidstinghen@gmail.com>
2019-07-22 00:19:17 -0300

    fet: component, state, props, babel, webpack

---
+++ src/components/TechList.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+import TechItem from "./TechItem";
+
+class TechList extends Component {
+  //default props
+  static defaultProps = {
+    tech: "Oculto"
+  };
+
+  //states
+  state = {
+    newTech: "",
+    techs: []
+  };
+
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  componentWillUnmount() {}
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          velue={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
